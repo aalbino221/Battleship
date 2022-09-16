bf3baf47086f84f5b62ceda63a93b559deafd2f0
aalbino221 <aalbino221@gmail.com>
2022-09-16 16:42:16 -0300

    main logic

---
+++ main.js
@@ -0,0 +1,19 @@
+(self["webpackChunkwebpack_boilerplate"] = self["webpackChunkwebpack_boilerplate"] || []).push([["main"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+alert("Um homem quando está em paz não quer guerra com ninguém")
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2VicGFjay1ib2lsZXJwbGF0ZS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJhbGVydChcIlVtIGhvbWVtIHF1YW5kbyBlc3TDoSBlbSBwYXogbsOjbyBxdWVyIGd1ZXJyYSBjb20gbmluZ3XDqW1cIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
